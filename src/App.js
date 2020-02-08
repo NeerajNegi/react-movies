d6ad6f8a3e2a8626f12997e27a32795ed8e0b881
Neeraj Negi <neeraj.negi@SLP160139-Neeraj.Dlink>
2020-02-08 22:14:15 +0530

    added hooks for search term in searchbar component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'shards-ui/dist/css/shards.min.css';
 
-import { getMovie } from './api';
-
 import Navbar from './Components/Navbar';
 
 function App() {
-
-  useEffect(() => {
-    getMovie(500).then(response => console.log(response.data));
-  });
 
   return (
     <Navbar/>
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './searchBar.css'
 
 export default function SearchBar() {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(searchTerm);
+    }
+
     return (
         <div className="searchbar">
-            <input className="search-input" type="text" placeholder="Search for a movie, actor or show..."/>
-            <button className="search-button"><i className="fa fa-search" aria-hidden="true"></i></button>
+            <form className="search-form" onSubmit={handleFormSubmit}>
+                <input 
+                    className="search-input" 
+                    type="text" 
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Search for a movie, actor or show..."/>
+                <button 
+                    type="submit"
+                    className="search-button">
+                    <i className="fa fa-search" aria-hidden="true"></i>
+                </button>
+            </form>
         </div>
     )
 }
